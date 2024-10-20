94a6683b4f636d8912cd7376aa31027861069df7
Yamid Yela <y.yamid@gmail.com>
2024-10-20 17:26:18 -0500

    Created Model User

---
+++ models/users.js
@@ -0,0 +1,45 @@
+import { Schema, model } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+
+const UserSchema = Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    nick: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    bio: String,
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "user"
+    },
+    image: {
+        type: String,
+        default: "default_user.png"
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Configurar el plugin de paginación de Mongo
+UserSchema.plugin(mongoosePaginate);
+
+export default model("User", UserSchema, "users");
