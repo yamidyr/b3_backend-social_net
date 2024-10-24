2b93f401b4602cba334686e0527ffc50426d98b2
Yamid Yela <y.yamid@gmail.com>
2024-10-23 19:55:47 -0500

    implemented saveFollow method

--- controllers/follow.js
+++ controllers/follow.js
@@ -10,11 +10,105 @@ export const testFollow = (req,res) => {
     });
 };
 
-// // Método para guardar un follow ( seguir a otro usuario )
-// export const saveFollow = async (req, res) => {
-//     try {
-        
-//     } catch (error) {
-        
-//     }
-// }
+// Método para guardar un follow ( seguir a otro usuario )
+export const saveFollow = async (req, res) => {
+    try {
+        // Obtener datos desde el body el usuario que se quiere seguir
+        const { followed_user } = req.body;
+
+        // Obtener el ID del usuario autenticado
+        const identity = req.user;
+
+        // Verificar si identity contiene al usuario autenticado
+        if(!identity || !identity.userId){
+            return res.status(400).send({
+                status: "error",
+                message: "No se ha proporcionado el usuario para realizar el folloing"
+            });
+        }
+
+        // Verificar si el usuario está intentando seguirse a sí mismo
+        if(identity.userId === followed_user){
+            return res.status(400).send({
+                status: "error",
+                message: "No puedes seguirte a ti mismo"
+            });
+        }
+
+        // Verificar si usuario a seguir existe
+        const followedUser = await User.findById(followed_user);
+        if(!followedUser){
+            return res.status(404).send({
+                status: "error",
+                message: "El usuario que intentas seguir no existe"
+            });
+        }
+
+        // Verificar si ya existe un seguimiento con los mismos usuarios
+        const existingFollow = await Follow.findOne({
+            following_user: identity.userId,
+            followed_user: followed_user
+        });
+
+        if( existingFollow ){
+            return res.status(400).send({
+                status: "error",
+                message: "Ya estás siguiendo a este usuario"
+            });
+        }
+
+        // Crear el objeto con el modelo follow
+        const newFollow = new Follow({
+            following_user: identity.userId,
+            followed_user: followed_user
+        });
+
+        // Guardar el objeto den la BD
+        const followStored = await newFollow.save();
+
+        // Verificar si se guardó correctamente en la BD
+        if(!followStored){
+            return res.status(500).send({
+                status: "error",
+                message: "No se ha podido seguir al usuario"
+            });
+        }
+
+        // Obtener el nombre y apellido del usuario seguido
+        const followedUserDetails = await User.findById(followed_user).select('name last_name');
+
+        if(!followedUserDetails){
+            return res.status(404).send({
+                status: "error",
+                message: "Usuario no encontrado"
+            });
+        }
+
+        // Combinar datos de follow y el followed user
+        const combinedFollowData = {
+            ...followStored.toObject(),
+            followedUser: {
+                name: followedUserDetails.name,
+                last_name: followedUserDetails.last_name
+            }
+        };
+
+        // Devolver respuesta
+        return res.status(200).json({
+            status: "success",
+            identity: req.user,
+            follow: combinedFollowData
+        });
+    } catch (error) {
+        if(error.code === 11000){
+            return res.status(400).send({
+                status: "error",
+                message: "Ya estás siguiendo a este usuario"
+            });
+        };
+        return res.status(500).send({
+            status: "error",
+            message: "Error al seguir al usuario"
+        })
+    }
+}
--- routes/follows.js
+++ routes/follows.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { testFollow } from "../controllers/follow.js";
+import { saveFollow, testFollow } from "../controllers/follow.js";
+import { ensureAuth } from "../middlewares/auth.js";
 
 const router = Router();
 
 // Definir rutas de follows
 router.get('/test-follow',testFollow);
+router.post('/follow',ensureAuth,saveFollow)
 
 // Exportar el Router
 export default router;
