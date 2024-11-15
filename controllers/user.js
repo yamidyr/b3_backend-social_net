e08eab4e35238eb98e8b577e2fcd0a66eb3397b9
Yamid Yela <y.yamid@gmail.com>
2024-11-14 20:51:55 -0500

    added method for counting followers

--- controllers/user.js
+++ controllers/user.js
@@ -374,3 +374,57 @@ export const avatar = async (req,res) => {
         });
     }
 }
+
+// Método para mostrar contador de seguidores y publicaciones
+export const counters = async (req, res) => {
+    try {
+      // Obtener el Id del usuario autenticado (token)
+      let userId = req.user.userId;
+  
+  
+      // Si llega el id a través de los parámetros en la URL tiene prioridad
+      if(req.params.id){
+        userId = req.params.id;
+      }
+  
+      // Obtener el nombre y apellido del usuario
+      const user = await User.findById(userId, { name: 1, last_name: 1});
+  
+  
+  
+      // Vericar el user
+      if(!user){
+        return res.status(404).send({
+          status: "error",
+          message: "Usuario no encontrado"
+        });
+      }
+  
+      // Contador de usuarios que yo sigo (o que sigue el usuario autenticado)
+      const followingCount = await Follow.countDocuments({ "following_user": userId });
+  
+      // Contador de usuarios que me siguen a mi (que siguen al usuario autenticado)
+      const followedCount = await Follow.countDocuments({ "followed_user": userId });
+  
+      // Contador de publicaciones del usuario autenticado
+      const publicationsCount = await Publication.countDocuments({ "user_id": userId });
+  
+      // Devolver los contadores
+      return res.status(200).json({
+        status: "success",
+        userId,
+        name: user.name,
+        last_name: user.last_name,
+        followingCount: followingCount,
+        followedCount: followedCount,
+        publicationsCount: publicationsCount
+      });
+  
+    } catch (error) {
+      console.log("Error en los contadores", error)
+      return res.status(500).send({
+        status: "error",
+        message: "Error en los contadores"
+      });
+    }
+  }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { avatar, listUsers, login, profile, register, testUser, updateUser, uploadAvatar } from "../controllers/user.js";
+import { avatar, counters, listUsers, login, profile, register, testUser, updateUser, uploadAvatar } from "../controllers/user.js";
 import { ensureAuth } from "../middlewares/auth.js";
 import multer from 'multer';
 import { CloudinaryStorage } from "multer-storage-cloudinary";
@@ -33,6 +33,7 @@ router.get('/list/:page?',ensureAuth, listUsers);
 router.put('/update',ensureAuth,updateUser);
 router.post('/upload-avatar', ensureAuth, uploads.single("file0"),uploadAvatar);
 router.get('/avatar/:id',avatar);
+router.get('/counters/:id?', ensureAuth, counters);
 
 // Exportar el Router
 export default router;
